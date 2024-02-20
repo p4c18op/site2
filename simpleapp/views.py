bd22a2a7095df8c0fa1750b56588aaee1d7f4f41
p4c18op <149690192+p4c18op@users.noreply.github.com>
2024-02-20 19:29:58 +0300

    Add files via upload

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -2,11 +2,13 @@ from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Product
 
+
 class ProductsList(ListView):
     model = Product
     ordering = 'name'
     template_name = 'products.html'
     context_object_name = 'products'
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
